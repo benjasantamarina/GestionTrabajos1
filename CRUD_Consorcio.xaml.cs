c6d9b2ae220d6bc551ede17c71e05a7843dbfb6d
benjasantamarina <santamarinabenjamin@gmail.com>
2023-08-02 22:03:43 -0300

    Agregar archivos de proyecto.

---
+++ CRUD_Consorcio.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GestionTrabajos1
+{
+    /// <summary>
+    /// Lógica de interacción para CRUD_Consorcio.xaml
+    /// </summary>
+    public partial class CRUD_Consorcio : Window
+    {
+        private DataClasses1DataContext dataContext;
+        public CRUD_Consorcio(DataClasses1DataContext context)
+        {
+            InitializeComponent();
+
+            dataContext = context;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+
+            string cuit = textcuit.Text;
+            string nom = textnombre.Text;
+            string dir = textdireccion.Text;
+            string sut = textsuterh.Text;
+
+            mainWindow.createConsorcio(cuit, nom, dir, sut);
+
+            MessageBox.Show("Registro ingresado con exito");
+
+            textcuit.Text = "";
+            textnombre.Text = "";
+            textdireccion.Text = "";
+            textsuterh.Text = "";
+        }
+
+
+        public void LoadConsorciosNames()
+        {
+            try
+            {
+                // Utilizar LINQ para obtener los nombres de la tabla 'consorcios'
+                var nombres = from consorcio in dataContext.Consorcio
+                              select consorcio.Nombre;
+
+                // Limpiar el ComboBox antes de cargar los nuevos datos
+                comboBoxConsorcios.Items.Clear();
+
+                // Cargar los nombres en el ComboBox
+                foreach (var nombre in nombres)
+                {
+                    comboBoxConsorcios.Items.Add(nombre);
+
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones, muestra un mensaje o realiza alguna acción en caso de error
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            LoadConsorciosNames();
+        }
+
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            (Application.Current.MainWindow as MainWindow)?.SetCRUDConsorcioInstance(null);
+        }
+
+       /* private void textcuit_LostFocus(object sender, RoutedEventArgs e)
+        {
+            string cuitText = textcuit.Text;
+
+            while (true)
+            {
+                // Verificar si el CUIT tiene 11 dígitos y los primeros 2 números cumplen con las condiciones requeridas
+                if (cuitText.Length == 11 && (cuitText.StartsWith("30") || cuitText.StartsWith("33")))
+                {
+                    try
+                    {
+                        long cuitNum = long.Parse(cuitText);
+                        break;
+                    }
+                    catch (FormatException ex)
+                    {
+                        MessageBox.Show($"{ex.Message}");
+
+                        textcuit.Text = "";
+
+                        break;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error, debe ingresar un CUIT correcto");
+
+                    textcuit.Text = "";
+
+                    break;
+                }
+
+            }
+
+        }*/
+
+        private void comboBoxConsorcios_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Obtener el nombre seleccionado en el ComboBox
+            string nombreSeleccionado = comboBoxConsorcios.SelectedItem as string;
+
+            if (nombreSeleccionado != null)
+            {
+                // Realizar la consulta para obtener los datos del consorcio seleccionado
+                var consorcioSeleccionado = dataContext.Consorcio.FirstOrDefault(consorcio => consorcio.Nombre == nombreSeleccionado);
+
+                // Mostrar los datos en los TextBox
+                if (consorcioSeleccionado != null)
+                {
+                    textcuit.Text = consorcioSeleccionado.CUIT;
+                    textnombre.Text = consorcioSeleccionado.Nombre;
+                    textdireccion.Text = consorcioSeleccionado.Direccion;
+                    textsuterh.Text = consorcioSeleccionado.ClaveSuterh;
+                }
+                else
+                {
+                    // Si el consorcio no se encontró en la base de datos, limpiar los TextBox
+                    textcuit.Text = "";
+                    textnombre.Text = "";
+                    textdireccion.Text = "";
+                    textsuterh.Text = "";
+                }
+            }
+        }
+        /*
+        private void textnombre_GotFocus(object sender, RoutedEventArgs e)
+        {
+            string cuit = textcuit.Text;
+           
+            if (cuit == "") { textcuit.Focus(); }
+        }*/
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace GestionTrabajos1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataClasses1DataContext dataContext;
+
+        CRUD_Consorcio windowCC;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string miConexion = ConfigurationManager.ConnectionStrings["GestionTrabajos1.Properties.Settings.AdministracionConnectionString"].ConnectionString;
+
+            dataContext = new DataClasses1DataContext(miConexion);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            // Crear una nueva instancia de CRUD_Consorcio solo si no existe o si ya se ha cerrado
+            if (windowCC == null)
+            {
+                windowCC = new CRUD_Consorcio(dataContext);
+                // Asignar el evento Closed para establecer la instancia en null cuando se cierre la ventana
+                windowCC.Closed += (s, args) => windowCC = null;
+
+                // Mostrar la ventana CRUD_Consorcio
+                windowCC.Show();
+            }
+
+            else
+            {
+                // Si ya existe una instancia activa, traerla al frente (focus)
+                windowCC.Focus();
+            }
+
+        }
+
+        public void SetCRUDConsorcioInstance(CRUD_Consorcio instance)
+        {
+            windowCC = instance;
+        }
+
+
+
+        public void createConsorcio(string cuit, string nom, string dir, string suterh)
+        {
+            Consorcio consorcio = new Consorcio(){ CUIT = cuit, Nombre = nom, Direccion = dir, ClaveSuterh = suterh };
+
+            dataContext.Consorcio.InsertOnSubmit(consorcio);
+
+            dataContext.SubmitChanges();
+        }
+
+
+    }
+}
